4edc63919d9a01fcb4f2355e5ba82dfa46050f58
alestor123 <alestoraldous@gmail.com>
2021-01-06 01:20:25 +0530

    added cli

--- cli.js
+++ cli.js
@@ -1,2 +1,6 @@
 #!/usr/bin/env node
 
+var clearLog = require('./App'),
+options = require('minimist')(process.argv.slice(2)),
+cron = options.t || options.time || '*/5 * * * *';
+if(options.s || options.s) clearLog(cron) , console.log('Started clear log cron input : '+cron)
